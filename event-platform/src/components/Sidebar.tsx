12f46c854321ecf1ce9191f2d1f8f428d65d80b3
Rafael <rafael.gcaldas01@gmail.com>
2022-07-25 23:43:10 -0300

    Format date

--- event-platform/src/components/Sidebar.tsx
+++ event-platform/src/components/Sidebar.tsx
@@ -35,7 +35,7 @@ export function Sidebar() {
       </span>
 
       <div className="flex flex-col gap-8">
-        {data?.lessons.map(lesson => {
+        {data?.lessons?.map(lesson => {
           return (
             <Lesson 
               key={lesson.id}
