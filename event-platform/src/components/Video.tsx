1193f435c2ce9544856af0ae7a95bc43a6514340
Rafael <rafael.gcaldas01@gmail.com>
2022-08-08 23:38:12 -0300

    Router

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -1,9 +1,51 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
+import { gql, useQuery } from "@apollo/client";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css';
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title
+      description
+      videoId
+      teacher {
+        bio
+        name
+        avatarURL
+      }
+    }
+  }
+`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug: string
+}
+
+export function Video(props: VideoProps) {
+
+  const {data} = useQuery(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug
+    }
+  })
+
+  console.log(data);
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
